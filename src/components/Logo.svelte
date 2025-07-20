de83065707cb202defde2ee78bb14bfeba0bd46a
oggunderscore <oggunderscore@gmail.com>
2025-07-20 04:48:06 -0700

    Fix logo

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -2,11 +2,12 @@
   export let className = '';
   export let width = '150';
   export let height = 'auto';
+  import { config } from '../lib/config';
 </script>
 
 <div class="flex items-center {className}">
   <img 
-    src="/src/assets/logo.png" 
+    src={`${config.imageBase}/logo.png`}
     alt="VT Electric Logo"
     width={width}
     height={height}
